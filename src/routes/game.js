a1c7658bcdd870d9ec5f4ada1208cc55fb85d63a
sanjana <66816050+Sanjana1202@users.noreply.github.com>
2023-07-16 15:29:20 +0200

    Updates Games Logic

--- src/routes/game.js
+++ src/routes/game.js
@@ -6,21 +6,22 @@ const session = require('express-session');
 
 const Router = express.Router();
 
-const isLogged = (request, response, next) => {
-    if (request.session.user) {
-        console.log('test');
+const isLogged = (req, res, next) => {
+    if (req.session.user) {
+        console.log('User is logged in');
         next();
     } else {
-        return response.status(401).json({ 'msg': 'You are not logged in!' });
+        return res.status(401).json({ 'msg': "Not logged in!" });
     }
 };
 
 function guessWord(guess, target) {
     if (guess.length !== target.length) {
-        throw new Error("Guessed word and target word must have the same length.");
+        throw new Error("Guess and target word must have the same length.");
     }
 
     let result = "";
+
     for (let i = 0; i < guess.length; i++) {
         if (guess[i] === target[i]) {
             result += "1";
@@ -34,143 +35,140 @@ function guessWord(guess, target) {
     return result;
 }
 
-function setDifficulty(request) {
-    const difficulty = request.body.difficulty;
+function setDifficulty(req) {
+    let result = true;
 
-    if (difficulty === 'difficult') {
-        request.session.difficulty = 3;
-    } else if (difficulty === 'medium') {
-        request.session.difficulty = 5;
-    } else if (difficulty === 'easy') {
-        request.session.difficulty = 8;
+    if (req.body.difficulty === 'hard') {
+        req.session.difficulty = 3;
+    } else if (req.body.difficulty === 'medium') {
+        req.session.difficulty = 5;
+    } else if (req.body.difficulty === 'easy') {
+        req.session.difficulty = 8;
     } else {
         return false;
     }
 
-    return true;
+    return result;
 }
 
-Router.post('/', isLogged, async (request, response) => {
-    if (!setDifficulty(request)) {
-        return response.status(400).json({
-            "msg": "Set the game difficulty before playing."
+Router.post('/', isLogged, async (req, res) => {
+    if (!setDifficulty(req)) {
+        return res.status(400).json({
+            "msg": "You must set game difficulty before playing"
         });
     }
 
+    const word = await WordModel.aggregate([{ $sample: { size: 1 } }]);
+
+    let game = new GameModel({
+        word: word[0]._id,
+        tries: [],
+        user: req.session.user._id
+    });
+
+    req.session.word = word[0].name;
+
     try {
-        const word = await WordModel.aggregate([{ $sample: { size: 1 } }]);
-
-        const game = new GameModel({
-            word: word[0]._id,
-            tries: [],
-            user: request.session.user._id
-        });
-
         await game.save();
+        req.session.gameId = game._id;
 
-        request.session.word = word[0].name;
-        request.session.gameId = game._id;
+        game = await GameModel.findById(game._id).populate('user').populate('word');
 
-        return response.status(200).json({
+        return res.status(200).json({
             "msg": "The secret word has a length of " + word[0].name.length
         });
     } catch (error) {
-        return response.status(500).json({
+        return res.status(500).json({
             "error": error.message
         });
     }
 });
 
-Router.get('/:id', async (request, response) => {
-    const { id } = request.params;
+Router.get('/:id', isLogged, async (req, res) => {
+    const { id } = req.params;
 
     try {
-        const game = await GameModel.findOne({ _id: id });
+        const game = await GameModel.findOne({ _id: id, user: req.session.user._id });
 
         if (!game) {
-            return response.status(404).json({
-                "error": "Game not found."
+            return res.status(404).json({
+                "error": "Game not found or not authorized"
             });
         }
 
-        return response.status(200).json({
+        return res.status(200).json({
             "msg": game
         });
     } catch (error) {
-        return response.status(500).json({
+        return res.status(500).json({
             "error": error.message
         });
     }
 });
 
-Router.post('/verif', isLogged, async (request, response) => {
-    if (typeof request.session.word === 'undefined') {
-        return response.status(400).json({
-            "msg": "You must create a game before playing."
+Router.post('/verif', isLogged, async (req, res) => {
+    if (typeof req.session.word === 'undefined') {
+        return res.status(403).json({
+            "msg": "You must create a game before playing"
         });
     }
 
-    if (request.session.difficulty === 0) {
-        return response.status(500).json({
-            "result": "You Lost!",
-            "tries": request.session.tries
+    if (req.session.difficulty === 0) {
+        return res.status(500).json({
+            "result": "You Lost !",
+            "tries": req.session.tries
         });
     }
 
-    const search = request.session.word;
-    const guess = request.body.word;
-
-    if (!guess) {
-        return response.status(400).json({
-            "msg": "You have to send the 'word' value."
-        });
-    }
+    let search = req.session.word;
+    let guess = req.body.word;
 
     if (search.length !== guess.length) {
-        return response.status(400).json({
-            "msg": "Your 'word' value must have a length of " + search.length,
-            "attemptsLeft": request.session.difficulty
+        return res.status(500).json({
+            "msg": "Your 'word' value must be " + search.length,
+            "attempts left": req.session.difficulty,
         });
     }
 
-    request.session.difficulty--;
-
-    const result = guessWord(guess, search);
-
-    try {
-        const newTry = new TryModel({
-            word: guess,
-            result: result
+    if (typeof guess === 'undefined') {
+        return res.status(500).json({
+            "msg": "You have to send 'word' value"
         });
+    }
 
-        const savedTry = await newTry.save();
+    req.session.difficulty -= 1;
 
-        const gamePlay = await GameModel.findById(request.session.gameId);
-        gamePlay.tries.push(savedTry._id);
-        await gamePlay.save();
+    let result = guessWord(guess, search);
 
-        if (guess === search) {
-            return response.status(200).json({
-                "result": "You Won!",
-                "attemptsLeft": request.session.difficulty,
-                "guess": guess,
-                "accuracy": result,
-                "game": await GameModel.findById(request.session.gameId).populate('tries')
-            });
-        }
+    const newTry = new TryModel({
+        word: req.body.word,
+        result: result
+    });
 
-        return response.status(500).json({
-            "result": "Wrong guess. Try again!",
-            "attemptsLeft": request.session.difficulty,
+    const savedTry = await newTry.save();
+
+    let gamePlay = await GameModel.findById(req.session.gameId);
+
+    gamePlay.tries.push(savedTry._id);
+    await gamePlay.save();
+
+    if (guess === search) {
+        return res.status(200).json({
+            "result": "You Won !",
+            "attempts left": req.session.difficulty,
             "guess": guess,
             "accuracy": result,
-            "game": await GameModel.findById(request.session.gameId).populate('tries')
-        });
-    } catch (error) {
-        return response.status(500).json({
-            "error": error.message
+            "game": await GameModel.findById(req.session.gameId).populate('tries')
         });
     }
+
+    return res.status(500).json({
+        "result": "Wrong guess. Try again !",
+        "attempts left": req.session.difficulty,
+        "guess": guess,
+        "accuracy": result,
+        "game": await GameModel.findById(req.session.gameId).populate('tries')
+    });
 });
 
 module.exports = Router;
