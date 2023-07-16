978c6614c8ebda4654c49a11837193d5e619160d
sanjana <66816050+Sanjana1202@users.noreply.github.com>
2023-07-16 18:50:20 +0200

    Store user data in a session upon successful login and Generate and return an access token upon successful login

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,93 +1,103 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
 
 const Router = express.Router();
 const saltRounds = 10;
 
 const UserModel = require('../models/user');
+const user = require('../models/user');
 
 const { generateAccessToken, generateRefreshToken } = require('../tools');
 
-// Register route
-Router.post('/register', async (req, res) => {
-    const { email, email_cfg, password, password_cfg, username, active } = req.body;
+Router.post('/register', async (request, response) => {
+    const {email, email_cfg, password, password_cfg, username, active} = request.body;
+
+    const hash = await bcrypt.hash(password, saltRounds);
+
+    const user = new UserModel({
+        email,
+        password: hash,
+        username,
+        active
+    });
 
     try {
-        const hashedPassword = await bcrypt.hash(password, saltRounds);
+        
+        await user.save();
 
-        const newUser = new UserModel({
-            email,
-            password: hashedPassword,
-            username,
-            active
-        });
-
-        await newUser.save();
-
-        return res.status(200).json({
-            "user": newUser
-        });
-    } catch (error) {
-        return res.status(500).json({
-            "error": error.message
-        });
-    }
-});
-
-// Login route
-Router.post('/login', async (req, res) => {
-    const { email, password } = req.body;
-
-    try {
-        const user = await UserModel.findOne({ email, active: true });
-
-        if (!user) {
-            return res.status(401).json({ error: 'User not authenticated!' });
-        }
-
-        const isPasswordValid = await bcrypt.compare(password, user.password);
-
-        if (!isPasswordValid) {
-            return res.status(401).json({ error: 'User not authenticated!' });
-        }
-
-        const accessToken = generateAccessToken(user._id);
-        const refreshToken = generateRefreshToken(user._id);
-
-        res.cookie('refreshToken', refreshToken, {
-            httpOnly: true,
-            maxAge: 30 * 24 * 60 * 60 * 1000 
-        });
-
-        return res.status(200).json({
-            "accessToken": accessToken,
+        return response.status(200).json({
             "user": user
         });
+
     } catch (error) {
-        return res.status(500).json({
+        return response.status(500).json({
             "error": error.message
         });
     }
+
 });
+
+Router.post('/login', async (request, response) => {
+    const {email, password} = request.body;
+
+    try {
+        
+        let user = await UserModel.findOne({
+            email,
+            active: true
+        });
+
+        if (user) {
+            let verif = await bcrypt.compare(password, user.password);
+
+            if (verif) {
+                request.session.user = user;
+
+                const accessToken = generateAccessToken(user._id)
+                const refeshToken = generateRefreshToken(user._id)
+
+                response.cookie('refreshtoken', refeshToken, {
+                    httpOnly: true,
+                    maxAge: 30*24*60*60*1000
+                });
+
+                return response.status(200).json({
+                    "accessToken": accessToken,
+                    "user": user
+                });
+            }
+        }
+
+        return response.status(500).json({
+            "error": "User not authenticated !"
+        });
+    } catch (error) {
+        return response.status(500).json({
+            "error": error.message
+        });
+    }
+
+});
+
 Router.get('/me', (request, response) => {
     return response.status(200).json({
         "user": request.session.user
     });
 })
-Router.get('/refreshToken', async (request, response) => {
+
+Router.get('/refresh-token', async (request, response) => {
     try {
         const rf_token = request.cookies.refreshtoken
 
-        if (!rf_token) return response.status(503).json({ msg: " User not authenticated !" });
+        if (!rf_token) return response.status(503).json({ msg: " user not authenticated !" });
 
         const decoded = jwt.verify(rf_token, `secret`)
 
-        if (!decoded) return response.status(503).json({ msg: "User not Authenticated !" })
+        if (!decoded) return response.status(503).json({ msg: "user not authenticated !" })
 
         const user = await UserModel.findById(decoded.id)
 
-        if (!user) return response.status(503).json({ msg: "User not authenticated !" })
+        if (!user) return response.status(503).json({ msg: "user not authenticated !" })
 
         const token = generateAccessToken(user._id)
 
@@ -96,9 +106,8 @@ Router.get('/refreshToken', async (request, response) => {
             user
         })
     } catch (error) {
-        return response.status(503).json({"msg": "User not authenticated !"});
+        return response.status(503).json({"msg": "user not authenticated !"});
     }
 });
 
 module.exports = Router;
-
