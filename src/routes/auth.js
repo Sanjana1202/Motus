b8c162ee441255b75d5a0a19fdb6bc7970f7fb65
sanjana <66816050+Sanjana1202@users.noreply.github.com>
2023-07-16 15:22:06 +0200

    Implement Login and Register routes using jwt

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,6 +1,5 @@
 const express = require('express');
-const bcrypt = require('bcryptjs');
-
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 const Router = express.Router();
@@ -10,81 +9,65 @@ const UserModel = require('../models/user');
 
 const { generateAccessToken, generateRefreshToken } = require('../tools');
 
-Router.post('/register', async (request, response) => {
-    const { email, password, username, active } = request.body;
+// Register route
+Router.post('/register', async (req, res) => {
+    const { email, email_cfg, password, password_cfg, username, active } = req.body;
 
     try {
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
 
-        const existingUser = await UserModel.findOne({ email });
-        if (existingUser) {
-            return response.status(409).json({ error: 'Email is already registered' });
-        }
-
-        const hash = await bcrypt.hash(password, saltRounds);
-
-        const user = new UserModel({
+        const newUser = new UserModel({
             email,
-            password: hash,
+            password: hashedPassword,
             username,
             active
         });
 
-        await user.save();
+        await newUser.save();
 
-        return response.status(201).json({
-            message: 'User registered successfully!!',
-            user: user
+        return res.status(200).json({
+            "user": newUser
         });
-
     } catch (error) {
-        return response.status(500).json({
-            error: error.message
+        return res.status(500).json({
+            "error": error.message
         });
     }
 });
 
-Router.post('/login', async (request, response) => {
-    const { email, password } = request.body;
+// Login route
+Router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
 
     try {
         const user = await UserModel.findOne({ email, active: true });
 
         if (!user) {
-            return response.status(401).json({
-                error: 'Invalid credentials'
-            });
+            return res.status(401).json({ error: 'User not authenticated!' });
         }
 
         const isPasswordValid = await bcrypt.compare(password, user.password);
 
         if (!isPasswordValid) {
-            return response.status(401).json({
-                error: 'Invalid credentials'
-            });
+            return res.status(401).json({ error: 'User not authenticated!' });
         }
 
         const accessToken = generateAccessToken(user._id);
         const refreshToken = generateRefreshToken(user._id);
 
-        response.cookie('refreshToken', refreshToken, {
+        res.cookie('refreshToken', refreshToken, {
             httpOnly: true,
-            maxAge: 30 * 24 * 60 * 60 * 1000 
+            maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days
         });
 
-        return response.status(200).json({
-            accessToken: accessToken,
-            user: {
-                _id: user._id,
-                email: user.email,
-                username: user.username
-            }
+        return res.status(200).json({
+            "accessToken": accessToken,
+            "user": user
         });
-
     } catch (error) {
-        return response.status(500).json({
-            error: error.message
+        return res.status(500).json({
+            "error": error.message
         });
     }
 });
 
-module.exports = Router;
